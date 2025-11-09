2dc295908b33ec613342aab015c3c59ce93cf498
MuhamadFaizNasai <faiznasai828@gmail.com>
2025-11-09 15:15:38 +0700

    tugas uts

---
+++ java/variabel.java
@@ -0,0 +1,21 @@
+public class variabel {
+    public static void main(String[] args) {
+        
+        // Data yang sesuai permintaan tugas
+        String namaLengkap = "Muhamad Faiz Nasai";
+        String tempatTanggalLahir = "Bogor, 20-januari-2006";
+        String jenisKelamin = "Laki-Laki";
+        String alamatLengkap = "Bogor, jalan raya cifor";
+        String noHp = "083133322368";
+        String penghasilanSebulan = "Rp 50.000.000";
+        
+        System.out.println("Assalamu'alaikum");
+        System.out.println("Izinkan saya memperkenalkan diri");
+        System.out.println("Nama Lengkap           : " + namaLengkap);
+        System.out.println("Tempat & Tanggal Lahir : " + tempatTanggalLahir);
+        System.out.println("Jenis Kelamin          : " + jenisKelamin);
+        System.out.println("Alamat Lengkap         : " + alamatLengkap);
+        System.out.println("No Hp                  : " + noHp);
+        System.out.println("Penghasilan Sebulan    : " + penghasilanSebulan);
+    }
+}
